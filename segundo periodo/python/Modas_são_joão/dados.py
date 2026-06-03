45a89194607ac7b1d3fa1c9ca88cb094416abcc8
Eduardo <edulima63@outlook.com>
2026-06-03 10:46:17 -0300

    inicição do projeto modas são joão

---
+++ segundo periodo/python/Modas_são_joão/dados.py
@@ -0,0 +1,17 @@
+''' aqui ficaram os tipos de dados guardados que serão usados na função main.py'''
+lista_registro = [] # guarda os dicts de registros
+
+# exemplo de estrutura dict
+
+#registro = {
+#    nome : str "nome"
+#    idade : int "idade"
+#    produto : {
+#       nome_produto :  str "nome"      
+#       tamanho_produto :  str "tamanho"
+ #      preco : float "preco"
+#        }
+#}
+lista_produtos = ["Chápeu","Vestido","Calça","Camisa"] #lista de produto
+categorias_roupas = ("Adulta","Juvenil") # lista imutavel de categoria de roupas
+tamanhos_fixos = ("P","M","G") # lista imutavel de tamanhos

f5dcf81e242db3f65e7833e00cee7e5bbb8d8e8d
Eduardo <edulima63@outlook.com>
2026-06-05 19:50:09 -0300

    terminei a função de criar registro

--- segundo periodo/python/Modas_são_joão/dados.py
+++ segundo periodo/python/Modas_são_joão/dados.py
@@ -1,5 +1,5 @@
 ''' aqui ficaram os tipos de dados guardados que serão usados na função main.py'''
-lista_registro = [] # guarda os dicts de registros
+dicionario_registros = {} # guarda os dicts de registros
 
 # exemplo de estrutura dict
 
@@ -12,9 +12,12 @@ lista_registro = [] # guarda os dicts de registros
  #      preco : float "preco"
 #        }
 #}
-categorias_roupas = ("adulta","juvenil") # lista imutavel de categoria de roupas
+categorias_roupas = ("adulto","juvenil") # lista imutavel de categoria de roupas
 
 tamanhos_fixos = ("P","M","G") # lista imutavel de tamanhos
+
+id_atual = 0 # registro de id dos produtos , começando em 0
+
 dicionario_produto = { #armazena os preços dos produtos disponiveis, #cada tamanho aumenta o preço em 20% transformando em int
     "chapeu" : {
         "preco_base_juvenil" : 25,
@@ -24,11 +27,11 @@ dicionario_produto = { #armazena os preços dos produtos disponiveis, #cada tama
         "preco_base_juvenil" : 30,
         "preco_base_adulto" : 45
     },
-    "Calça" : {
+    "calça" : {
         "preco_base_juvenil" : 25,
         "preco_base_adulto" : 40
     },
-    "Camisa" : {
+    "camisa" : {
         "preco_base_juvenil" : 25,
         "preco_base_adulto" : 40
     },
--- segundo periodo/python/Modas_são_joão/functions.py
+++ segundo periodo/python/Modas_são_joão/functions.py
@@ -1,7 +1,15 @@
 '''Aqui serão armazenadas as funções CRUD que serão usadas no arquivo principal main.py'''
 
 def criar_novo_registro():
-    from dados import dicionario_produto,tamanhos_fixos #importa os dados para tratamento de erros
+    '''cria registro de venda de acordo com nome da pessoa,roupa escolhida,tamanho escolhido ,idade da pessoa 
+    e armazena em um dict
+    
+    Arg: none
+
+    Returns: Dict de registros atualizado
+
+    '''
+    from dados import dicionario_produto,tamanhos_fixos, id_atual,dicionario_registros #importa os dados para tratamento de erros
 
     nome = input("digite o nome do cliente: ")
     idade = input("digite a idade do cliente: ")
@@ -31,4 +39,38 @@ def criar_novo_registro():
     else: 
         print("a idade digitada não é contemplada pelos produto da loja tente novamente!!!") #caso a idade não seja adulta e nem infantil
         return 0
+
+    if linha_produto == "juvenil": #aplica preco com base na linha jovem ou adulta
+        preco = dicionario_produto[produto]["preco_base_juvenil"]
+
+    elif linha_produto == "adulto":
+        preco = dicionario_produto[produto]["preco_base_adulto"]
+
+    else:
+        print("ops erro ao definir preço")
+
+    if tamanho_roupa == "M": #se for p o preco é o mesmo do preco base, se for maior aumenta em 20% em tamanho M e 40# em tamanho G
+        preco = preco * 1.2
+    elif tamanho_roupa == "G":
+        preco = preco * 1.4 
+    
+    nome = nome.strip()
+    nome = nome.casefold()
+    nome = nome.title() #metodos para Formatação de string
+
+    dicionario_registros[f"registro_id_{id_atual + 1}"] = { #criação do dict de registro
+        "nome" : nome,
+        "idade" : idade,
+        "id" : id_atual + 1,
+        "tamanho" : tamanho_roupa,
+        "linha" : linha_produto,
+        "preco" : preco
+    }
+    print(dicionario_registros)
+    return 0
+
+
+
+    
+    
 
